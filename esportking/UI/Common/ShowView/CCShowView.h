f1ba333561abaf7f14172a8074b232cc5e874694
jaycechen <jaycechen@tencent.com>
2018-03-06 20:10:31 +0800

    添加展示button

---
+++ esportking/UI/Common/ShowView/CCShowView.h
@@ -0,0 +1,29 @@
+//
+//  CCShowView.h
+//  esportking
+//
+//  Created by jaycechen on 2018/3/6.
+//  Copyright © 2018年 wan353. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define CCShowViewSize  CGSizeMake(CCPXToPoint(96), CCPXToPoint(96))
+
+typedef enum : NSUInteger {
+    SHOWSTATUS_UP = 1,
+    SHOWSTATUS_DOWN,
+} SHOWSTATUS;
+
+@protocol CCShowViewDelegate <NSObject>
+
+- (void)didClickShowView:(SHOWSTATUS)status;
+
+@end
+
+@interface CCShowView : UIView
+
+- (void)setDelegate:(id<CCShowViewDelegate>)del;
+- (void)setCurrentStatus:(SHOWSTATUS)status location:(CGPoint)point animated:(BOOL)animated;
+
+@end
---
+++ esportking/UI/Common/ShowView/CCShowView.m
@@ -0,0 +1,87 @@
+//
+//  CCShowView.m
+//  esportking
+//
+//  Created by jaycechen on 2018/3/6.
+//  Copyright © 2018年 wan353. All rights reserved.
+//
+
+#import "CCShowView.h"
+
+@interface CCShowView ()
+
+@property (assign, nonatomic) SHOWSTATUS currentStatus;
+@property (weak  , nonatomic) id<CCShowViewDelegate> delegate;
+
+@property (strong, nonatomic) UIButton *button;
+
+@end
+
+@implementation CCShowView
+
+- (instancetype)init
+{
+    return [self initWithFrame:CGRectMake(0, 0, CCShowViewSize.width, CCShowViewSize.height)];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame])
+    {
+        [self setupUI];
+    }
+    return self;
+}
+
+- (void)setupUI
+{
+    [self addSubview:self.button];
+    [self.button setFrame:self.frame];
+    [self.button setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+}
+
+- (void)setDelegate:(id<CCShowViewDelegate>)del
+{
+    _delegate = del;
+}
+
+- (void)setCurrentStatus:(SHOWSTATUS)status location:(CGPoint)point animated:(BOOL)animated
+{
+    _currentStatus = status;
+    UIImage *image = nil;
+    switch (status) {
+        case SHOWSTATUS_UP:
+        {
+            image = CCIMG(@"Select_Up");
+        }
+            break;
+        case SHOWSTATUS_DOWN:
+        {
+            image = CCIMG(@"Select_Down");
+        }
+            break;
+        default:
+            break;
+    }
+    [self.button setImage:image forState:UIControlStateNormal];
+}
+
+#pragma mark - action
+- (void)onClickButton:(UIButton *)button
+{
+    [self.delegate didClickShowView:_currentStatus];
+}
+
+#pragma mark - getter
+- (UIButton *)button
+{
+    if (!_button)
+    {
+        _button = [UIButton new];
+        [_button setBackgroundImage:CCIMG(@"Select_BG") forState:UIControlStateNormal];
+        [_button addTarget:self action:@selector(onClickButton:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _button;
+}
+
+@end
